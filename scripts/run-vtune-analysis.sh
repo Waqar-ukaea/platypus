8a8c9541a0b6b20ffcf18fb267dfa5586b58a2c7
waqar-ukaea <waqar.butt@ukaea.uk>
2024-10-28 19:05:59 +0000

    Updated script to create tar of all results summaries.html

--- scripts/run-vtune-analysis.sh
+++ scripts/run-vtune-analysis.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+mkdir -p /opt/platypus/vtune-results
 resultsDir=/opt/platypus/vtune-results
-workdir=/opt/platypus/test/tests/kernels
+regressionTestDir=/opt/platypus/test/tests/kernels
 analysisType="hotspots"
 
 cd $regressionTestDir || exit 1
-
+shopt -s nullglob
 for inputFile in $regressionTestDir/*.i; do
-    [ -e "$inputFile" ] || echo "No input files avaialble in regression testsuite. Check Platypus bu>
     testName="$(basename "$inputFile" .${inputFile##*.})"
     
     echo "------------------------------------------------------------------"
     echo "Running Intel VTune $analysisType on $testName regression test" 
     echo "------------------------------------------------------------------"
 
-    vtune -collect hotspots -strategy=:trace:trace -result-dir="$resultsDir/$testName" /opt/platypus>
     cd $resultsDir
-    vtune -report summary -r "$resultsDir/$testName" -format=html > "${testName}_${analysisType}_sum>
+    vtune -collect hotspots -strategy=:trace:trace -result-dir="$resultsDir/$testName" /opt/platypus/platypus-opt -i $inputFile
+    vtune -report summary -r "$resultsDir/$testName" -format=html > "${testName}_${analysisType}_summary.html"
     tar -cvf "${testName}_${analysisType}_results".tar $testName 
 done
 
-
+tar -cvf "regression_test_${analysisType}_summaries.tar" *.html 
