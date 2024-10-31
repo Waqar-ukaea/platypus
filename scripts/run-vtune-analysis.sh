697fa1f45e38e339d41479bdb92bf587b044772f
Waqar Butt <114666466+Waqar-ukaea@users.noreply.github.com>
2024-10-31 17:51:39 +0000

    Fixed missing end of loop causing script to fail

--- scripts/run-vtune-analysis.sh
+++ scripts/run-vtune-analysis.sh
@@ -8,19 +8,23 @@ resultsDir=/opt/platypus/vtune-results
 regressionTestDir=/opt/platypus/test/tests/kernels
 analysisType="hotspots"
 
-
-cd $regressionTestDir || exit 1
+cd "$regressionTestDir" || exit 1
 shopt -s nullglob
-for inputFile in $regressionTestDir/*.i; do
+for inputFile in "$regressionTestDir"/*.i; do
     testName="$(basename "$inputFile" .${inputFile##*.})"
     
     echo "------------------------------------------------------------------"
     echo "Running Intel VTune $analysisType on $testName regression test" 
     echo "------------------------------------------------------------------"
 
-    cd $resultsDir
-    vtune -collect "$analysisType" -strategy=:trace:trace -result-dir="$resultsDir/$testName" /opt/platypus/platypus-opt -i $inputFile
+    cd "$resultsDir"
+    vtune -collect "$analysisType" -strategy=:trace:trace -result-dir="$resultsDir/$testName" /opt/platypus/platypus-opt -i "$inputFile"
     vtune -report summary -r "$resultsDir/$testName" -format=html > "${testName}_${analysisType}_summary.html"
-    tar -cvf "${testName}_${analysisType}_results".tar $testName 
-tar -cvf "regression_test_${analysisType}_full.tar" *.tar # tar full results into compressed file for artifact upload
-tar -cvf "regression_test_${analysisType}_summaries.tar" *.html # tar results summaries into compressed file for artifact upload
+    tar -cvf "${testName}_${analysisType}_results.tar" "$testName"
+done
+
+# tar full results into compressed file for artifact upload
+tar -cvf "regression_test_${analysisType}_full.tar" *.tar
+
+# tar results summaries into compressed file for artifact upload
+tar -cvf "regression_test_${analysisType}_summaries.tar" *.html
